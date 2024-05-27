4282a2f685ca2199dd9a6b058966a4edae2c109b
Lukinskai <31217624+Lukinskai@users.noreply.github.com>
2024-05-27 10:28:23 -0400

    breeds updated, icons added,bugs fixes

--- js/index.js
+++ js/index.js
@@ -1,174 +1,194 @@
+const imagesSection = document.querySelector('.images');
+const breedsSection = document.querySelector('.breeds');
+const radios = document.querySelector('.header__switch-model');
 
-//creating a footer
-const footer = document.createElement("footer");
-//footer.textContent = "My Footer";
+const modelToSectionMap = new Map();
+modelToSectionMap.set('images', imagesSection);
+modelToSectionMap.set('breeds', breedsSection);
 
-//get the body element
+const modelToEndpointMap = new Map();
+modelToEndpointMap.set('images', '/images/search?limit=10&breed_ids=beng,abys,esho&');
+// modelToEndpointMap.set('images', '/images/search?limit=30&');
+modelToEndpointMap.set('breeds', '/breeds?');
 
-const body = document.body;
+const apiUrl = 'https://api.thecatapi.com/v1';
+const apiKey = 'live_MyLymXXGPoDA6Df7x8M9paTQTu5DVI6v5nHoTSGC0SojLHxM4QIkrFI5EKy7ubYi';
 
-// Append the footer to the body
+let currentModel = 'images';
+const currentDataArray = [];
 
-body.appendChild(footer);
-
-//-----
-
-// Date 
-const today = new Date();
-
-//year
-const thisYear = today.getFullYear();
-
-//footer from DOM
-const footer2 = document.querySelector('footer');
-
-//copyright
-const copyright = document.createElement('p');
-
-//name and year
-
-copyright.innerHTML = "Super CAT &copy;" + thisYear;
-
-//append the copy to the footer
-
-footer.appendChild(copyright);
-
-///Skills 
-
-const skills =["CATScript","CTML","CTSS","Cava","Kit"];
-
-const skillsSection = document.getElementById('skills');
-
-const skillsList = skillsSection.querySelector('ul');
-
-for (let i=0; i < skills.length; i++){
-    const skill = document.createElement('li');
-
-skill.innerText = skills[i];
-
-skillsList.appendChild(skill);
-
-}
-
-///Message FORM
-
-const messageForm = document.forms.leave_message;
-
-
-messageForm.addEventListener('submit', function(event) {
-
-    event.preventDefault();
-
-    const usersName = event.target.usersName.value;
-    const usersEmail = event.target.usersEmail.value;
-    const usersMessage = event.target.usersMessage.value;
-
-    console.log('Name:', usersName);
-    console.log('Email:', usersEmail);
-    console.log('Message:', usersMessage);
-
-    ////
-
- const messageSection = document.getElementById('messages');
-
-
- const messageList = messageSection.querySelector('ul');
-
-
- const newMessage = document.createElement('li');
-
-
- newMessage.innerHTML = `
-     <a href="mailto:${usersEmail}">${usersName}</a>: 
-     <span>${usersMessage}</span>`;
-
- const removeButton = document.createElement('button');
-
- removeButton.innerText = 'remove';
- removeButton.type = 'button';
-
-
- removeButton.addEventListener('click', function() {
-
-     const entry = removeButton.parentNode;
-     
-     entry.remove();
- });
-
-
- newMessage.appendChild(removeButton);
-
-
- messageList.appendChild(newMessage);
-
-    ////
-
-     event.target.reset();
-
+radios.addEventListener('change', (event) => {
+  const target = event.target;
+  switch (target.id) {
+    // I could use if else here since there are only two options,
+    // but decided to use switch so that it is possible to add more models later
+    case 'images':
+      currentModel = 'images';
+      imagesSection.style.display = 'flex';
+      breedsSection.style.display = 'none';
+      break;
+    case 'breeds':
+      currentModel = 'breeds';
+      imagesSection.style.display = 'none';
+      breedsSection.style.display = 'flex';
+      break;
+  }
+  generatePageElements(currentModel);
 });
 
-///lesson-15
+const generatePageElements = (model) => {
+  // get current section's container
+  const container = modelToSectionMap.get(model).querySelector('.section__container');
+  // clear current data array
+  currentDataArray.length = 0;
+  // add Loading element/spinner
+  showLoadingSpinner(container);
+  // fetch data
+  const requestUrl = `${apiUrl}${modelToEndpointMap.get(model)}api_key=${apiKey}`;
+  fetchData(requestUrl, model, container);
+}
 
-// 'MY_GITHUB_USERNAME'
-const myUsername = 'Lukinskai';
-
-// Make a GET request to fetch repository data
-fetch(`https://api.github.com/users/${myUsername}/repos`)
+async function fetchData(url, model, container) {
+    console.log("url  ", url);
+    console.log("model  ", model);
+    
+  fetch(url)
     .then(response => {
-        // Check if the request was successful
-        if (!response.ok) {
-            throw new Error('Network response was not ok, check Username');
-        }
-
-        return response.json();
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+      return response.json();
     })
     .then(data => {
-       
-        console.log(data);
-       
-        const repositories = data;
-    
-        console.log('Repositories:', repositories);
+        console.log("data  ", data);
 
-        /// Repos in List
-  
-        const projectSection = document.getElementById('Projects');
-   
-        const projectList = projectSection.querySelector('ul');
-
-       
-        for (let i = 0; i < repositories.length; i++) {
-         
-            const project = document.createElement('li');
-            
-            project.innerText = repositories[i].name;
-       
-            projectList.appendChild(project);
-        }
+      data.forEach(el => appendDataToArray(el, model));
+      // create html elements and append them to the section container
+      container.innerHTML = '';
+      currentDataArray.forEach(el => container.appendChild(createDataCard(el, model)));
     })
     .catch(error => {
-  
-        console.error('Error:', error);
-  
-        alert('An error occurred. Please try again later.');
+      console.error('An error occurred:', error);
     });
+}
 
+const showLoadingSpinner = (container) => {
+  container.innerHTML = '';
+  const spinnerDiv = document.createElement('div');
+  spinnerDiv.classList.add('loading-spinner');
+  let circle;
+  for (let i = 0; i < 12; i++) {
+    circle = document.createElement('div');
+    circle.classList.add('circle');
+    spinnerDiv.appendChild(circle);
+  }
+  container.appendChild(spinnerDiv);
+}
 
+const appendDataToArray = (data, model) => {
+  switch (model) {
+    case 'images':
+      const breed = data.breeds[0] ? data.breeds[0].name : '';
+      const imageUrl = data.url;
+      if (!imageUrl.endsWith('.gif')) { // do not include gifs
+        currentDataArray.push({ 'breed': breed, 'imageUrl': imageUrl });
+      }
+      break;
+    case 'breeds':
+      const name = data.name;
+      const bredFor = data.bred_for;
+      const breedGroup = data.breed_group;
+      const lifeSpan = data.life_span;
+      const temperament = data.temperament;
+      const origin = data.origin;
+      // const image = data.image.url;
+      const weight = data.weight.imperial;
+      const description = data.description;
 
-    /// TheCATAPI
+      // const height = data.height.imperial;
+      currentDataArray.push({
+        'breed': name,
+        //'imageUrl': image,
+        'bredFor': bredFor,
+        'breedGroup': breedGroup,
+        'lifeSpan': lifeSpan,
+        'temperament': temperament,
+        'origin': origin,
+        'weight': weight,
+        'description': description
 
-    const headers = new Headers({
-        "Content-Type": "application/json",
-        "x-api-key": "DEMO-API-KEY"
+        // 'height': height
       });
-      
-      var requestOptions = {
-        method: 'GET',
-        headers: headers,
-        redirect: 'follow'
-      };
-      
-      fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));        
+  }
+}
+
+const createDataCard = (data, model) => {
+  const card = document.createElement('div');
+  switch (model) {
+    case 'images':
+      card.classList.add('images__card');
+      const img = document.createElement('img');
+      img.src = data.imageUrl;
+      img.alt = `Picture of ${data.breed ? data.breed : 'a cat'}`;
+      card.appendChild(img);
+      const text = document.createElement('h3');
+      text.innerText = `Breed: ${data.breed ? data.breed : 'no breed information'}`;
+      card.appendChild(text);
+      break;
+    case 'breeds':
+      card.classList.add('breeds__card');
+      const breedImg = document.createElement('img');
+      breedImg.src = data.imageUrl;
+      breedImg.alt = `Picture of ${data.breed}`;
+      card.appendChild(breedImg);
+      const breed = document.createElement('h4');
+      breed.innerText = data.breed;
+      card.appendChild(breed);
+      if (data.bredFor) {
+        const bredFor = document.createElement('p');
+        bredFor.innerHTML = `<span>Bred for:</span> ${data.bredFor}`;
+        card.appendChild(bredFor);
+      }
+      if (data.breedGroup) {
+        const breedGroup = document.createElement('p');
+        breedGroup.innerHTML = `<span>Breed group:</span> ${data.breedGroup}`;
+        card.appendChild(breedGroup);
+      }
+
+      if (data.description) {
+        const description = document.createElement('p');
+        description.innerHTML = `<span>Description:</span> ${data.description}`;
+          card.appendChild(description);
+      }
+      if (data.weight) {
+        const weight = document.createElement('p');
+        weight.innerHTML = `<span>Weight(lb):</span> ${data.weight}`;
+        card.appendChild(weight);
+      }
+      // if (data.height) {
+      //   const height = document.createElement('p');
+      //   height.innerHTML = `<span>Height(in):</span> ${data.height}`;
+      //   card.appendChild(height);
+      // }
+      if (data.lifeSpan) {
+        const lifeSpan = document.createElement('p');
+        lifeSpan.innerHTML = `<span>Life span:</span> ${data.lifeSpan}`;
+        card.appendChild(lifeSpan);
+      }
+      if (data.temperament) {
+        const temperament = document.createElement('p');
+        temperament.innerHTML = `<span>Temperament:</span> ${data.temperament}`;
+        card.appendChild(temperament);
+      }
+      if (data.origin) {
+        const origin = document.createElement('p');
+        origin.innerHTML = `<span>Origin:</span> ${data.origin}`;
+        card.appendChild(origin);
+      }
+
+  }
+  return card;
+}
+
+
+generatePageElements(currentModel);
